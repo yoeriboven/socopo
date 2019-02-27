bb97fd26f0b7f590904b260208f62fee12af7420
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-02-27 18:05:22 +0100

    Slightly more cleanup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Profile;
-use App\Libraries\Instagram\InstagramDownloader;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,23 +11,10 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+
+
     public function instagramTester()
     {
-        $profiles = Profile::all();
-
-        foreach ($profiles as $profile) {
-            $api   = new InstagramDownloader();
-            $api->setUserName($profile->username);
-
-            $feed = $api->getFeed();
-
-            echo '<pre>';
-            print_r($feed);
-            echo '</pre>';
-            echo "<img src='{$feed->medias[0]->displaySrc}' />";
-
-            // Avoid 429 Rate limit from Instagram
-            sleep(.5);
-        }
+        (new \App\InstagramCommand)->handle();
     }
 }
