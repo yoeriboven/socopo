f57aa1de1c18a5619a172dbd25594e7ce204cd5c
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-03-07 15:10:19 +0100

    Add profiles to posts without fetching the profiles multiple times

--- app/Post.php
+++ app/Post.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     /**
-     * The relationships to always eager-load.
-     *
-     * @var array
-     */
-    protected $with = ['profile'];
-
-    /**
      * The profile this post belongs to
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -12,27 +12,22 @@ class PostRepository
      * @param  User $user
      * @return \Illuminate\Database\Eloquent\Collection;
      */
-    // public function forUser($user = null)
-    // {
-    //     // Use the given user or the authenticated user
-    //     $user = $user ?? auth()->user();
-
-    //     // Get the IDs of the profiles the user follows
-    //     $profiles = $user->profiles->pluck('id');
-
-    //     // Fetch the latest posts, restricted to those by profiles followed by the user
-    //     return Post::whereIn('profile_id', $profiles)->latest()->get();
-    // }
-    //
     public function forUser($user = null)
     {
         // Use the given user or the authenticated user
         $user = $user ?? auth()->user();
 
         // Get the IDs of the profiles the user follows
-        $profiles = $user->profiles->pluck('id');
+        $profiles = $user->profiles()->get()->keyBy('id');
 
-        // Fetch the latest posts, restricted to those by profiles followed by the user
-        return Post::whereIn('profile_id', $profiles)->latest()->get();
+        // Get all the posts which belong to the profiles
+        $posts = Post::whereIn('profile_id', $profiles->modelKeys())->latest()->get();
+
+        // Add the profiles to the posts
+        $posts->transform(function ($post) use ($profiles) {
+            return $post->setRelation('profile', $profiles->get($post->profile_id));
+        });
+
+        return $posts;
     }
 }
