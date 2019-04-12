4fcc9bb8fe2470438ce00ac1a1f4ebe0e9873f27
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-04-12 14:23:40 +0200

    Moved validation rules to UserDetails model

--- app/Http/Requests/UserDetailsRequest.php
+++ app/Http/Requests/UserDetailsRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Validation\Rule;
+use App\UserDetails;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UserDetailsRequest extends FormRequest
@@ -14,13 +14,6 @@ class UserDetailsRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name' => 'required|min:3|max:50',
-            'vat_id' => 'nullable|vat_number',
-            'address' => 'required|min:5|max:100',
-            'postal' => 'required|max:15',
-            'city' => 'required|max:50',
-            'country' => ['required', Rule::in(array_keys(config('countries')))]
-        ];
+        return UserDetails::getValidationRules();
     }
 }
