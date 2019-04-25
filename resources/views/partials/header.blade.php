44008810dcab817cb337c675541cf9a29585f27e
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-04-25 13:04:54 +0200

    Changed the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,45 +7,17 @@
                         <h2>Commenter</h2>
                     </a>
                     <div class="d-flex order-lg-2 ml-auto">
-                        <div class="dropdown">
-                            <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown">
-                                <span class="avatar" style="background-image: url('demo/images/37.jpg');"></span>
-                                <span class="ml-2 d-none d-lg-block">
-                                    <span class="text-default">Jane Pearson</span>
-                                    <small class="text-muted d-block mt-1">Administrator</small>
-                                </span>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                <a class="dropdown-item" href="#">
-                                    <i class="dropdown-icon fe fe-user"></i> Profile
-                                </a>
-
-                                <a class="dropdown-item" href="/settings">
-                                    <i class="dropdown-icon fe fe-settings"></i> Settings
-                                </a>
-
-                                <a class="dropdown-item" href="#">
-                                <span class="float-right"><span class="badge badge-primary">6</span></span>
-                                    <i class="dropdown-icon fe fe-mail"></i> Inbox
-                                </a>
-
-                                <a class="dropdown-item" href="#">
-                                    <i class="dropdown-icon fe fe-send"></i> Message
-                                </a>
-
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">
-                                    <i class="dropdown-icon fe fe-help-circle"></i> Need help?
-                                </a>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        <div class="nav-item d-none d-md-flex">
+                          <a href="/settings"><i class="fe fe-settings"></i></a>
+                        </div>
+                        <div class="nav-item d-none d-md-flex">
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    <i class="dropdown-icon fe fe-log-out"></i> Sign out
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
+                                <i class="fe fe-log-out"></i>
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         </div>
                     </div>
                     <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#mainMenu">
