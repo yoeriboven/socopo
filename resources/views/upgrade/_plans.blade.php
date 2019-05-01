ce08be67bf83671149d8c7051809854efd2d9aa9
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-05-01 18:13:08 +0200

    Styled the plans on upgrade page

--- resources/views/upgrade/_plans.blade.php
+++ resources/views/upgrade/_plans.blade.php
@@ -2,26 +2,75 @@
     <div class="card-body">
 		<div class="row">
 			<div class="col-3">
-				<h4 class="card-title">Step 1: The plan</h4>
+				<h4 class="card-title">Step 1: Pick a plan</h4>
 			</div>
 			<div class="col-9">
 				<div class="form-group">
-					<div class="form-label">Inline Radios</div>
-					<div class="custom-controls-stacked">
-						<label class="custom-control custom-radio custom-control-inline">
-							<input type="radio" class="custom-control-input" name="plan" value="plan_1" checked>
-							<span class="custom-control-label">Plan 1</span>
-						</label>
-						<label class="custom-control custom-radio custom-control-inline">
-							<input type="radio" class="custom-control-input" name="plan" value="plan_2">
-							<span class="custom-control-label">Plan 2</span>
-						</label>
-						<label class="custom-control custom-radio custom-control-inline">
-							<input type="radio" class="custom-control-input" name="plan" value="plan_3">
-							<span class="custom-control-label">Plan 3</span>
-						</label>
+					<div class="row">
+						<div class="col-sm-6 col-lg-4">
+							<label class="pricing-label">
+								<input type="radio" class="custom-control-input" name="plan" value="plan_1">
+
+								<div class="card">
+									<div class="card-body text-center">
+										<div class="card-category">Start-up</div>
+										<div class="display-3 my-4">$20</div>
+										<ul class="list-unstyled leading-loose">
+											<li><strong>25</strong> Profiles</li>
+											<li><i class="fe fe-check text-success mr-2" aria-hidden="true"></i> Sharing Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Design Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Private Messages</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Twitter API</li>
+										</ul>
+									</div>
+								</div>
+							</label>
+						</div>
+
+						<div class="col-sm-6 col-lg-4">
+							<label class="pricing-label">
+								<input type="radio" class="custom-control-input" name="plan" value="plan_2">
+
+								<div class="card">
+									<div class="card-body text-center">
+										<div class="card-category">Company</div>
+										<div class="display-3 my-4">$50</div>
+										<ul class="list-unstyled leading-loose">
+											<li><strong>100</strong> Profiles</li>
+											<li><i class="fe fe-check text-success mr-2" aria-hidden="true"></i> Sharing Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Design Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Private Messages</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Twitter API</li>
+										</ul>
+									</div>
+								</div>
+							</label>
+						</div>
+
+						<div class="col-sm-6 col-lg-4">
+							<label class="pricing-label">
+								<input type="radio" class="custom-control-input" name="plan" value="plan_3">
+
+								<div class="card">
+									<div class="card-body text-center">
+										<div class="card-category">Agency</div>
+										<div class="display-3 my-4">$70</div>
+										<ul class="list-unstyled leading-loose">
+											<li><strong>Unlimited</strong> Profiles</li>
+											<li><i class="fe fe-check text-success mr-2" aria-hidden="true"></i> Sharing Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Design Tools</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Private Messages</li>
+											<li><i class="fe fe-x text-danger mr-2" aria-hidden="true"></i> Twitter API</li>
+										</ul>
+									</div>
+								</div>
+							</label>
+						</div>
 					</div>
+					<p class="text-muted">*All plans are monthly subscriptions</p>
 				</div>
+
+
 			</div>
 		</div>
 	</div>
