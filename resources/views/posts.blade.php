5805f3d44f17da935f5691c841e9f6ca5133516d
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-05-06 15:22:23 +0200

    Added alert

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,6 +11,12 @@
 	</div>
 @endif
 
+@if (Session::has('status'))
+	<div class="alert alert-icon alert-success full-width" role="alert">
+		<i class="fe fe-check mr-2" aria-hidden="true"></i> {{ Session::get('status') }}
+	</div>
+@endif
+
 <p class="text-right font-weight-bold"><a href="#profiles" id="modalOpener" data-toggle="modal" data-target="#profilesModal">Manage profiles</a></p>
 
 @if (count($posts) == 0)
