6dcc7416acde13a6e906a3f7761572725d7436db
Yoeri Boven <yoeriboven@MacBook-Air-van-Yoeri.local>
2019-05-28 17:06:25 +0200

    Name on user is not required

--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -10,23 +10,6 @@ class UserRegistrationTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function a_name_is_not_required()
-    {
-        $attributes = [
-            'email' => 'test@yoeri.me',
-            'password' => 'test_password',
-            'password_confirmation' => 'test_password'
-        ];
-
-        $this->post('register', $attributes);
-
-        $this->assertDatabaseHas('users', [
-            'email' => 'test@yoeri.me',
-            'name' => null
-        ]);
-    }
-
-    /** @test */
     public function a_user_needs_to_be_verified()
     {
         $user = factory('App\User')->create(['email_verified_at' => null]);
