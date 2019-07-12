112e1572ee20e87b33ba19ee7c62e644af2f2676
Yoeri Boven <yoeriboven@msn.com>
2019-07-12 17:13:02 +0200

    Added contact link

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container text-center mb-3">
 	<p class="text-muted">
-		&#169; 2019 - {{ config('app.name', 'Socopo') }}
+		&#169; 2019 - <a href="mailto:yoeri@yoeri.me?subject=Socopo.xyz%20Contact">{{ config('app.name', 'Socopo') }}</a>
 	</p>
 </div>
