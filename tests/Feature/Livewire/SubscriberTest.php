640acc8304621e62d0077018a9bea8efb7e5eb9a
Yoeri Boven <yoeriboven@msn.com>
2020-09-23 18:01:47 +0200

    Added Livewire tests

--- tests/Feature/Livewire/SubscriberTest.php
+++ tests/Feature/Livewire/SubscriberTest.php
@@ -12,6 +12,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SubscriberTest extends TestCase
 {
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_emits_an_event_when_it_finishes()
+    {
+        $this->signIn();
+
+        $userDetails = factory('App\UserDetails')->make();
+
+        Livewire::test(Subscriber::class)
+            ->set('plan_id', Plans::first()->id)
+            ->set('userDetails', $userDetails->toArray())
+            ->call('subscribe')
+            ->assertEmitted('readyForPaddle');
+    }
+
     /** @test */
     public function it_requires_a_plan_id()
     {
@@ -32,5 +48,36 @@ class SubscriberTest extends TestCase
             ->set('plan_id', 'invalid_plan_id')
             ->call('subscribe')
             ->assertHasErrors('plan_id');
+    }
+
+    /** @test */
+    public function it_saves_the_user_details()
+    {
+        $user = $this->signIn();
+
+        $userDetails = factory('App\UserDetails')->make();
+
+        Livewire::test(Subscriber::class)
+            ->set('plan_id', Plans::first()->id)
+            ->set('userDetails', $userDetails->toArray())
+            ->call('subscribe');
+
+        $this->assertEquals($user->details->name, $userDetails->name);
+    }
+
+    /** @test */
+    public function it_mounts_with_the_existing_user_details()
+    {
+        $user = $this->signIn();
+
+        $userDetails = factory('App\UserDetails')->make()->toArray();
+        $user->details()->update($userDetails);
+
+        Livewire::test(Subscriber::class)
+            ->assertSet('userDetails.name', $user->details->name)
+            ->assertSet('userDetails.address', $user->details->address)
+            ->assertSet('userDetails.postal', $user->details->postal)
+            ->assertSet('userDetails.city', $user->details->city)
+            ->assertSet('userDetails.country', $user->details->country);
     }
 }
